3a72e7801fdd6261835e171575016c89a6f1582f
lucyd <sreenatha.dev@gmail.com>
2013-07-02 14:48:24 +0530

    Migrated weatherapp listener to stem

--- weather/weatherapp/listener.py
+++ weather/weatherapp/listener.py
@@ -1,4 +1,4 @@
-"""A module for listening to TorCtl for new consensus events. When one occurs,
+"""A module for listening to Tor for new consensus events. When one occurs,
 initializes the checker/updater cascade in the updaters module."""
 
 import sys, os
@@ -7,41 +7,28 @@ import socket
 
 from config import config
 from weatherapp import updaters
-from TorCtl import TorCtl
+from stem.control import EventType, Controller
 
 #very basic log setup
 logging.basicConfig(format = '%(asctime) - 15s (%(process)d) %(message)s',
                     level = logging.DEBUG, filename = 'log/weather.log')
 
-class MyEventHandler(TorCtl.EventHandler):
-    """Extends C{TorCtl.EventHandler} so that C{updaters.run_all} is called
-    when a NEWCONSENSUS event is received.
+def newconsensus_listener(event):
+    """Call C{updaters.run_all()} when a NEWCONSENSUS event is received.
+
+    @param event: The NEWCONSENSUS event. Not used by the function.
     """
-    def new_consensus_event(self, event):
-        """Call C{updaters.run_all()} when a NEWCONSENSUS event is received.
-
-        @param event: The NEWCONSENSUS event. Not used by the function,
-                      but included so that this overrides 
-                      C{TorCtl.EventHandler.new_consensus_event}
-        """
-
-        logging.info('Got a new consensus. Updating router table and ' + \
+    
+    logging.info('Got a new consensus. Updating router table and ' + \
                      'checking all subscriptions.')
-        updaters.run_all()
+    updaters.run_all()
 
 def listen():
-    """Sets up a connection to TorCtl and launches a thread to listen for
+    """Sets up a connection to Tor and initializes a controller to listen for
     new consensus events.
     """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ctrl_host = '127.0.0.1'
-    ctrl_port = config.control_port
-    sock.connect((ctrl_host, ctrl_port))
-    ctrl = TorCtl.Connection(sock)
-    ctrl.launch_thread(daemon=0)
+    ctrl = Controller.from_port(config.control_port)
     ctrl.authenticate(config.authenticator)
-    ctrl.set_event_handler(MyEventHandler())
-    ctrl.set_events([TorCtl.EVENT_TYPE.NEWCONSENSUS])
+    ctrl.add_event_listener(newconsensus_listener, EventType.NEWCONSENSUS)
     print 'Listening for new consensus events.'
     logging.info('Listening for new consensus events.')
-
