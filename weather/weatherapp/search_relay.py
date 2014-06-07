ab5acaf3db1da213ecd75bb324cfdb38cfbba3d3
Sreenatha Bhatlapenumarthi <sreenatha.dev@gmail.com>
2014-06-08 02:27:12 +0530

    #11956 Using the Onionoo wrapper in the relay-search script

--- weather/weatherapp/search_relay.py
+++ weather/weatherapp/search_relay.py
@@ -1,65 +1,37 @@
 """
-This module takes a keyword as input and display's results of matching 
+This module takes a keyword as input and display's results of matching
 relays using onionoo's search functionality.
 """
 
-import urllib2
-import urllib
-import json
-
-#query_data is a global dictionary
-#key is search query
-#value is [last-modified timestamp, dict-formatted summary document]
-query_data = {}
+from onionoo_wrapper import *
 
 
-def print_relays(result_dict):
+def print_relays(relays):
     """ Print the nicknames, fingerprints and IP addresses of relays """
-    for relay in result_dict['relays']:
-        if 'n' not in relay.keys():
+
+    for relay in relays:
+        if relay.nickname is None:
             relay_nick = "Unnamed"
         else:
-            relay_nick = relay['n']
+            relay_nick = relay.nickname
         print('Nickname:%s ' % (relay_nick)),
-        print('Fingerprint:%s ' % (relay['f'])),
-        print('IP address(es):%s ' % (relay['a']))
+        print('Fingerprint:%s ' % (relay.fingerprint)),
+        print('IP address(es):%s ' % (relay.addresses))
 
 
 def search_relays(query):
-    """ Given a search query, return corresponding relay-details """
+    """ Given a search query, returns corresponding relay-details """
 
     global query_data
 
     # Onionoo search parameters
-    base_URL = "https://onionoo.torproject.org/summary"
     params = {
         'type': 'relay',
         'search': query
     }
-
-    # Build the appropriate request
-    request_URL = base_URL + '?' + urllib.urlencode(params)
-    request = urllib2.Request(url=request_URL)
-    if query in query_data.keys():
-        request.add_header('If-Modified-Since', query_data[query][0])
-
-    try:
-        # Send the request to Onionoo
-        result = urllib2.urlopen(request)
-        result_dict = json.loads(result.read())
-
-        # Update the global dictionary
-        last_modified = result.info().getheader("Last-Modified")
-        query_data[query] = [last_modified, result_dict]
-
-        print_relays(result_dict)
-
-    except urllib2.HTTPError, error:
-        if error.code == 304:
-            # Not Modified
-            print_relays(query_data[query][1])
-        else:
-            print("Error " + str(error.code) + ": " + error.reason)
+    req = objects.OnionooRequest()
+    resp = req.get_response('summary', params)
+    print_relays(resp.document.relays)
 
 
 if __name__ == "__main__":
