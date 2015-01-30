bb366faa330b1b75fa63bd2a1eb7d5df6360d5d4
meejah <meejah@meejah.ca>
2015-01-30 01:08:12 -0700

    proper arg for utests

--- integ/test_unit.py
+++ integ/test_unit.py
@@ -2,4 +2,4 @@ from subprocess import check_call
 
 def test_run_unit_tests_in_vagrant():
     '''This just runs the unit-tests on-box'''
-    check_call(['vagrant', 'ssh', '-c', 'python /home/weather/opt/current/weather/manage.py test'])
+    check_call(['vagrant', 'ssh', '-c', 'python /home/weather/opt/current/weather/manage.py test weatherapp'])
